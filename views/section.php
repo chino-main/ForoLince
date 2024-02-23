92a3efd5e2036ee6cfda7fc5de80d3304813e5ee
Yahel Ramirez Felix <yahelrafe@gmail.com>
2024-02-23 08:53:35 -0700

    Update section.php

--- views/section.php
+++ views/section.php
@@ -4,5 +4,5 @@
 
 <section id="section-test">
     <h1>Seccion de prueba</h1>
-    <button class="color-outline ripple_effect" data-flip-id="animate" onclick="toggleWindow('#window-example_big')">Abrir ventana grande</button>
+    <button class="color-outline ripple_effect" data-flip-id="animate" onclick="toggleWindow('#window-example_big')">Abrir ventana cola</button>
 </section>
